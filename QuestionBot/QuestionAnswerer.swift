7bfdd2064670b277a7d58fbfcad7c35602f2a6d4
murad <muradkv@gmail.com>
2019-06-17 00:34:27 +0300

    Add some Bot answer functionality

--- QuestionBot/QuestionAnswerer.swift
+++ QuestionBot/QuestionAnswerer.swift
@@ -11,7 +11,27 @@ import Foundation
 class MyQuestionAnswerer {
     
     func responseTo(question: String) -> String {
-        return "?"
+        
+        let lowerQuestion = question.lowercased()
+        
+        if lowerQuestion.hasPrefix("hello") {
+            return "Of course, not hello to you! Go fuck yourself."
+        } else if lowerQuestion.hasPrefix("when") {
+            return "When Earth down"
+        } else if lowerQuestion.hasPrefix("what") {
+            return "What...what. Look at this another stupid human. Please, can you shut the fuck up your mouth, leather bag."
+        } else {
+            
+            let defaultAnswer = question.count % 2
+            
+            if defaultAnswer == 0 {
+                return "I don't understand, you piece of shit"
+            } else {
+                return "Oh, yes. Of course I have an aswer. SUCK MY DICK"
+            }
+        
+        }
+        
     }
     
 }
