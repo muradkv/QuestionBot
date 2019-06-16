f394faafe61a8f365a6669b637fbec981efce00a
murad <muradkv@gmail.com>
2019-06-16 23:14:49 +0300

    Add UI

--- QuestionBot/ViewController.swift
+++ QuestionBot/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var responseLabel: UILabel!
+    @IBOutlet var questionField: UITextField!
+    @IBOutlet var askButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func askButtonTapped(_ sender: Any) {
+    }
+    
+}
 
+extension ViewController: UITextFieldDelegate {
+    
+    @IBAction func editingChanged(_ sender: UITextField) {
+    }
 }
 
